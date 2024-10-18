fc6cb9563edadc5618edb39954a7fbd7b19e6fa1
marco <marcowang05@gmail.com>
2024-10-18 18:53:32 +0200

    refactor and cleaning folder structure

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # multimedia-data-security
+```
+multimedia-data-security/
+├── common/
+│   ├── attacks.py                 # Handle the attacks that can be performed on the image (embedded)
+│   ├── detection.py               # Handle the detection of our watermark
+│   ├── embedding.py               # Handle the embedding of our watermark in the image
+│   ├── utility.py                 # Handle utility function that is common in more parts
+|
+|
+├── out/                           # Contain the output of our notebook
+├── sample_imgs/                   # Contain input images sample
+|
+├── ammhackaty.npy                 # Watermark
+|
+├── test_bordi.ipynb               # Contain the output of our notebook
+├── test_make.ipynb                # Contain input images sample
+├── test_tenko.ipynb               # Watermark
+└── README.md                      # Project documentation
+```
---
+++ common/detection.py
@@ -0,0 +1,73 @@
+import numpy as np
+import pywt
+from common.utility import create_perceptual_mask,get_locations,modular_alpha
+
+
+def extract_watermark(subband, watermarked_subband, layer, theta, alpha=0.5, v='multiplicative'):
+    # Create perceptual mask for the subband
+    mask = create_perceptual_mask(subband)
+    abs_subband, sign, locations = get_locations(subband)
+    abs_watermarked, _, _ = get_locations(watermarked_subband)
+    mark_size = 1024
+
+    extracted_mark = []
+    
+    # Loop through each location (except the first one)
+    for idx, loc in enumerate(locations[1:mark_size+1]):
+        x = locations[idx][0]
+        y = locations[idx][1]
+        
+        if v == 'additive':
+            # Reverse the additive watermarking process to extract the mark
+            extracted_value = (abs_watermarked[loc] - abs_subband[loc]) / (modular_alpha(layer, theta, alpha) * mask[x][y] + 1e-6)
+        elif v == 'multiplicative':
+            # Reverse the multiplicative watermarking process to extract the mark
+            extracted_value = ((abs_watermarked[loc] / abs_subband[loc]) - 1 + 1e-6) / (modular_alpha(layer, theta, alpha) * mask[x][y] + 1e-6)
+        
+        # Append the extracted mark value
+        extracted_mark.append(extracted_value)
+    
+    return np.array(extracted_mark)
+
+
+
+def detection(image, watermarked, alpha, v='multiplicative'):
+    #ori_dct = dct(dct(image,axis=0, norm='ortho'),axis=1, norm='ortho')
+    LL0_or, (LH0_or, HL0_or, HH0_or) = pywt.dwt2(image, 'haar')
+    LL1_or, (LH1_or, HL1_or, HH1_or) = pywt.dwt2(LL0_or, 'haar')
+    LL2_or, (LH2_or, HL2_or, HH2_or) = pywt.dwt2(LL1_or, 'haar')
+    LL3_or, (LH3_or, HL3_or, HH3_or) = pywt.dwt2(LL2_or, 'haar')
+     
+
+    #wat_dct = dct(dct(watermarked,axis=0, norm='ortho'),axis=1, norm='ortho')
+    LL0_w, (LH0_w, HL0_w, HH0_w) = pywt.dwt2(watermarked, 'haar')
+    LL1_w, (LH1_w, HL1_w, HH1_w) = pywt.dwt2(LL0_w, 'haar')
+    LL2_w, (LH2_w, HL2_w, HH2_w) = pywt.dwt2(LL1_w, 'haar')
+    LL3_w, (LH3_w, HL3_w, HH3_w) = pywt.dwt2(LL2_w, 'haar')
+    
+    extracted_wms = []
+    # we cant embed in the layer 3 because size is 1023 and not 1024
+
+    # extracted_wms.append(extract_watermark(LH3_or, LH3_w, 3, 0, alpha=alpha, v=v))
+    # extracted_wms.append(extract_watermark(HL3_or, HL3_w, 3, 2, alpha=alpha, v=v))
+    # extracted_wms.append(extract_watermark(HH3_or, HH3_w, 3, 1, alpha=alpha, v=v))
+
+
+def similarity(X,X_star):
+    #Computes the similarity measure between the original and the new watermarks.
+    s = np.sum(np.multiply(X, X_star)) / (np.sqrt(np.sum(np.multiply(X, X))) * np.sqrt(np.sum(np.multiply(X_star, X_star))))
+    return s
+
+def compute_thr(sim, mark, mark_size=1024, N=1000):
+    SIM = np.zeros(N)
+    for i in range(N):
+        r = np.random.uniform(0.0, 1.0, mark_size)
+        SIM[i] = (similarity(mark, r))
+    SIMs = SIM.copy()
+    SIM.sort()
+    t = SIM[-1]
+    T = t + (0.1*t)
+    print('Threshold: ', T)
+    return T, SIMs
+
+
